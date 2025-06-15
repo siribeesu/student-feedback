a198fd2bee9bd14df134ebdb5ad733e2dbb303d0
siri beesu <siribeesu07@gmail.com>
2025-06-15 19:09:25 +0530

    Updated API endpoint to production backend

--- scirpt.js
+++ scirpt.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', async (e) => {
   const data = Object.fromEntries(formData.entries());
 
   try {
-    const response = await fetch('https://student-feedback-8wgk.onrender.com/api/feedback', {
+    const response = await fetch('https://student-feedback-backend-1-g1fl.onrender.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
